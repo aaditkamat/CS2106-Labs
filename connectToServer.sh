1ceafbbe25e1b0e96d10e19da8eb54dc349c1f47
aaditkamat <aadit.k12@gmail.com>
2018-11-03 22:11:54 +0800

    Modify script to connect to Ubuntu AWS EC2 instance

--- connectToServer.sh
+++ connectToServer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ssh -i "~/Desktop/ubuntu.pem" ubuntu@ec2-13-229-125-163.ap-southeast-1.compute.amazonaws.com
+ssh -i ubuntu.pem ubuntu@ec2-13-229-125-163.ap-southeast-1.compute.amazonaws.com
 
